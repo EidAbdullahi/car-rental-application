f8e2c0b7f421b014ade4d8e0ba00f282c5d61039
Eid Abdulla <eidabdullahi10.com>
2025-03-17 10:31:36 +0200

    commit changes

--- carrentalapp/urls.py
+++ carrentalapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +9,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('rent/', views.rent_car, name='rent_car'),
     path('return/', views.return_car, name='return_car'),
+    path('login/', views.login_view, name='login'),  # Update here
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
--- carrentalapp/views.py
+++ carrentalapp/views.py
@@ -4,6 +4,39 @@ from .models import CarRental, CarReturn
 from django.contrib.auth.models import User
 from .models import UserProfile
 from .forms import CarRentalForm, CarReturnForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponse
+
+def test_view(request):
+    return HttpResponse("Django is working!")
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to home page
+    else:
+        form = UserCreationForm()
+    return render(request, 'carrentalapp/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'carrentalapp/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
 def profile_view(request):
